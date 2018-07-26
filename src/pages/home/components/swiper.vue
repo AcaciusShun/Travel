4cfd1dab6492ef060ad1824a51e267d19ffd1e8b
bailongctui <bailongctui@gmail.com>
2018-07-27 02:35:07 +0800

    update ESlint Bug

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -17,7 +17,7 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop:true
+        loop: true
       },
       swiperList: [{
         id: '0001',
