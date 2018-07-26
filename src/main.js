48ae0c179065322dc0cdccae32de2c3c915844d6
bailongctui <bailongctui@gmail.com>
2018-07-27 01:40:22 +0800

    用新路径来表示

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import './assets/styles/border.css' // 解决1px问题
-import './assets/styles/iconfont.css' // 引入iconfont
-
+import 'styles/reset.css'
+import 'styles/border.css' // 解决1px问题
+import 'styles/iconfont.css' // 引入iconfont
 
 import FastClck from 'fastclick'
 
