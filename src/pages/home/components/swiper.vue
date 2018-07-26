34ad88fdc8a4f9353719d5aa073f4e4f70c48733
bailongctui <bailongctui@gmail.com>
2018-07-27 02:31:15 +0800

    swiper组件 开发

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop:true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/a1/41a802abfc4f0202.jpg_750x200_9f0cf69c.jpg'
+      },
+      {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/e7/d70d0830a9941b02.jpg_750x200_f8c7d2ad.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  //穿透样式
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #ffffff
+  //预留出swiper位置
+  .wrapper
+    width: 100%
+    overflow hidden
+    height:0
+    padding-bottom : 26.67%
+    background gray
+    .swiper-img
+      width 100%
+</style>
