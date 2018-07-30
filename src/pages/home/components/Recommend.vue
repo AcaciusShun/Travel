8255465e9b22db4edc4b6b4e22ef6e0deda59e73
bailongctui <bailongctui@gmail.com>
2018-07-30 22:40:46 +0800

    实现 热销推荐组件

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,18 +1,87 @@
 <template>
   <div>
-    <div class="recommend-title">热销推荐</div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_150x150_21259c73.jpg',
+        title: '苏州乐园森林水世界',
+        desc: 'xxxxxxxxxxxxxxxxxxx'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_150x150_21259c73.jpg',
+        title: '苏州乐园森林水世界',
+        desc: 'xxxxxxxxxxxxxxxxxxx'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_150x150_21259c73.jpg',
+        title: '苏州乐园森林水世界',
+        desc: 'xxxxxxxxxxxxxxxxxxx'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_150x150_21259c73.jpg',
+        title: '苏州乐园森林水世界',
+        desc: 'xxxxxxxxxxxxxxxxxxx'
+      }]
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
-.recommend-title
+ @import "~styles/mixins.styl"
+
+.title
   margin-top .2rem
   line-height .8rem
-  background #111
+  background #ccc
   text-indent .2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+  /*background #25a4bb*/
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding .1rem
+  .item-info
+    flex 1
+    padding .1rem
+    min-width 0
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color gray
+      ellipsis()
+    .item-button
+      line-height .44rem
+      background coral
+      padding 0 .2rem
+      border-radius .06rem
+      margin-top .16rem
+      color white
 </style>
