3e8e26ebb975f14ff499f8505d321b133d9e1a32
bailongctui <bailongctui@gmail.com>
2018-07-30 13:49:52 +0800

    图标区域逻辑实现

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,55 +1,112 @@
 <template>
 <div class="icons">
-  <div class="icon">
-    <div class="icon-img">
-      <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-    </div>
-    <p class="icon-desc">热门景点</p>
-  </div>
+  <swiper>
+    <swiper-slide>
+      <div class="icon" v-for="item of iconList" :key="item.id">
+        <div class="icon-img">
+          <img class="icon-img-content" :src="item.imgUrl" alt="">
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </swiper-slide>
+  </swiper>
 </div>
 </template>
 <script>
 export default {
-
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+          desc: '古镇'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '文化古迹'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '苏州必旅'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc: '水上玩乐'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png',
+          desc: '汽车票'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/17/99402a22ce4af302.png',
+          desc: '苏州园林'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '拙政林'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '虎丘'
+        },
+        {
+          id: '00010',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+          desc: '南浔古镇'
+        }]
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-    overflow: hidden
-    width 100%
+  .icons >>> .swiper-container
     height 0
     padding-bottom 50%
     /*background: #010133*/
-    .icon
-      position relative
-      overflow hidden
-      float left
-      width 25%
-      height 0
-      padding-bottom 25%
-      /*background #25a4bb*/
-      .icon-img
-        position: absolute
-        top 0
-        left 0
-        right 0
-        bottom .44rem
-        box-sizing border-box
-        padding .1rem
-        /*background: #cacaca*/
-        .icon-img-content
-          display block
-          margin 0 auto
-          height 100%
-      .icon-desc
-        position: absolute
-        left 0
-        right 0
-        bottom 0
-        height .44rem
-        line-height .44rem
-        color $darkTextColor
-        text-align center
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    /*background #25a4bb*/
+    .icon-img
+      position: absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      /*background: #cacaca*/
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position: absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      color $darkTextColor
+      text-align center
 </style>
