87e9ce4172bf0c41aea3773c5257b59af7ba23f2
bailongctui <bailongctui@gmail.com>
2018-08-03 23:50:26 +0800

    install vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import FastClck from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import store from './store/index'
 
 import 'styles/reset.css'
 import 'styles/border.css' // 解决1px问题
@@ -19,6 +20,7 @@ Vue.use(VueAwesomeSwiper)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
